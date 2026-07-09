1ac852b06a3d978411b20eccfc193b7e3c55af2a
longg1708 <longabc932@gmail.com>
2026-07-10 00:01:50 +0700

    [DE191106] feat: BE car browsing

--- VivuCarServer/API/Configurations/ServiceConfiguration.cs
+++ VivuCarServer/API/Configurations/ServiceConfiguration.cs
@@ -14,6 +14,7 @@ public static class ServiceConfiguration
         services.AddScoped<IUserSecurityStateService, UserSecurityStateService>();
         services.AddScoped<IBookingService, BookingService>();
         services.AddScoped<IPaymentService, PaymentService>();
+        services.AddScoped<ICarService, CarService>();
         services.AddHostedService<API.HostedServices.BookingExpirationHostedService>();
 
         return services;
